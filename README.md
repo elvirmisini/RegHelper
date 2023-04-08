6a25c71c4684f5f08d37bf46a7bf8736a5e317fb
Elvir Misni <elvirmisini@gmail.com>
2023-04-09 01:21:48 +0200

    Reg helper

--- README.md
+++ README.md
@@ -52,21 +52,63 @@ Lower cases, Upper cases, numbers so in a total length of 6 chars.
 
 ```javascript
 const isValid = isUrlRegEx('https://github.com/elvirmisini/RegHelper')
+```
+```javascript
 const isValid = isUsSSNRegEx('222-22-2222')
+```
+For USA SSN
+```javascript
 const isValid = isCardRegEx('4111111111111111')
+```
+Credit Card
+```javascript
 const isValid = isIPRegEx('192.158.1.38')
+```
+IP address
+```javascript
 const isValid = isUsZipCodeRegEx('99577-0727')
+```
+USA Zip code
+```javascript
 const isValid = isDateMmDdYyyySlashRegEx('12/22/2023')
+```
+Date format  mm/dd/yyyy
+```javascript
 const isValid = isDateDdMmYyyySlashRegEx('22/12/2023')
+```
+Date format dd/mm/yyyy
+```javascript
 const isValid = isDateYyyyMmDdSlashRegEx('2023/12/22')
+```
+Date format yyyy/mm/dd
+```javascript
 const isValid = isDateMmDdYySlashRegEx('12/22/22')
+```
+Date format mm/dd/yy
+```javascript
 const isValid = isDateYyyyMmDdHyphenRegEx('2023-12-12')
+```
+Date format yyyy-mm-dd
+```javascript
 const isValid = isDateMmDdYyyyHyphenRegEx('12-12-2023')
+```
+Date format mm-dd-yyyy
+```javascript
 const isValid = isDateDdMmYyyyHyphenRegEx('22-12-2023')
+```
+Date format dd-mm-yyyy
+```javascript
 const isValid = isHeksadeximalColorCodeRegEx('#123456')
+```
+Hexadecimal Color code
+```javascript
 const isValid = isMACAdressRegEx('00-B0-D0-63-C2-26')
+```
+MAC address
+```javascript
 const isValid = isSubnetMaskRegEx('255.255.255.0')
 ```
+SubnetMask
 
 ## Contributing
 If you find a bug or want to contribute to the development of RegHelper, please submit a pull request on the GitHub repository.
---
+++ index.js
@@ -0,0 +1,126 @@
+function isEmailRegEx(email){
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function isPasswordRegEx(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/;
+    return passwordRegex.test(password);
+}
+
+function isPhoneNumberRegEx(phoneNumber) {
+    
+    const phoneRegex = /^\+\d{1,3}\d{8,}$/;
+    return phoneRegex.test(phoneNumber);
+}
+
+function isUsernameRegEx(username, minLength, maxLength) {
+    const usernameRegex = new RegExp(`^[a-zA-Z0-9][a-zA-Z0-9_.-]{${minLength - 1},${maxLength - 1}}$`);
+    return usernameRegex.test(username);
+  }
+
+function isCodeRegEx(code, length) {
+    const codeRegex = new RegExp(`^[a-zA-Z0-9]{${length}}$`);
+    return codeRegex.test(code);
+  }
+    
+function isUrlRegEx(url) {
+    const urlRegex = /^(https?:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/.*)?$/;
+    return urlRegex.test(url);
+  }
+
+function isUsSSNRegEx(SSN) {
+    const SSNRegex = /^\d{3}-\d{2}-\d{4}$/;
+    return SSNRegex.test(SSN);
+  }
+
+function isCardRegEx(Card) {
+    const CardRegex =  /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/;
+    return CardRegex.test(Card);
+  }
+
+function isCardRegEx(Card) {
+    const CardRegex =  /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/;
+    return CardRegex.test(Card);
+  }
+  
+function isIPRegEx(IP) {
+    const IPRegex =  /^([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])$/;
+    return IPRegex.test(IP);
+  }
+
+function isUsZipCodeRegEx(ZIP) {
+    const ZIPRegex =  /^\d{5}(?:[-\s]\d{4})?$/;
+    return ZIPRegex.test(ZIP);
+  }
+  
+function isDateMmDdYyyySlashRegEx(date) {
+    const dateRegex =  /^(0?[1-9]|1[0-2])\/(0?[1-9]|[12][0-9]|3[01])\/((19|20)\d{2})$/;
+    return dateRegex.test(date);
+  }
+function isDateDdMmYyyySlashRegEx(date) {
+    const dateRegex =  /^(0[1-9]|[12]\d|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+    return dateRegex.test(date);
+  }
+function isDateYyyyMmDdSlashRegEx(date) {
+    const dateRegex =  /^\d{4}\/(0[1-9]|1[0-2])\/(0[1-9]|[12]\d|3[01])$/;
+    return dateRegex.test(date);
+  }
+function isDateMmDdYySlashRegEx(date) {
+    const dateRegex =  /^(0[1-9]|1[0-2])\/(0[1-9]|[12]\d|3[01])\/\d{2}$/;
+    return dateRegex.test(date);
+  }
+  
+function isDateYyyyMmDdHyphenRegEx(date) {
+    const dateRegex =  /^(?:(?:19|20)[0-9]{2})[-\/.](?:0?[1-9]|1[0-2])[-\/.](?:0?[1-9]|[12][0-9]|3[01])$/;
+    return dateRegex.test(date);
+  }
+
+function isDateDdMmYyyyHyphenRegEx(date) {
+    const dateRegex =  /^(0[1-9]|[12]\d|3[01])-(0[1-9]|1[0-2])-\d{4}$/;
+    return dateRegex.test(date);
+  }
+
+function isDateMmDdYyyyHyphenRegEx(date) {
+    const dateRegex =  /^(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])-\d{4}$/;
+    return dateRegex.test(date);
+  }
+
+function isHeksadeximalColorCodeRegEx(colorCode) {
+    const colorCodeRegex =  /^#(?:[0-9a-fA-F]{3}){1,2}$/;
+    return colorCodeRegex.test(colorCode);
+  }
+    
+function isMACAdressRegEx(MACAddress) {
+    const MACAddressRegex =  /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/;
+    return MACAddressRegex.test(MACAddress);
+  }
+
+function isSubnetMaskRegEx(subnetMask) {
+    const subnetMaskRegex = /^((128|192|224|240|248|252|254)\.0\.0\.0)|(255\.(0|128|192|224|240|248|252|254)\.0\.0)|(255\.255\.(0|128|192|224|240|248|252|254)\.0)|(255\.255\.255\.(0|128|192|224|240|248|252|254))$/;
+    return subnetMaskRegex.test(subnetMask);
+  }
+    
+
+module.exports={
+    isEmailRegEx,
+    isPasswordRegEx,
+    isPhoneNumberRegEx,
+    isUsernameRegEx,
+    isCodeRegEx,
+    isUrlRegEx,
+    isUsSSNRegEx,
+    isCardRegEx,
+    isIPRegEx,
+    isUsZipCodeRegEx,
+    isDateMmDdYyyySlashRegEx,
+    isDateDdMmYyyySlashRegEx,
+    isDateYyyyMmDdSlashRegEx,
+    isDateMmDdYySlashRegEx,
+    isDateYyyyMmDdHyphenRegEx,
+    isDateDdMmYyyyHyphenRegEx,    
+    isDateMmDdYyyyHyphenRegEx,
+    isHeksadeximalColorCodeRegEx,
+    isMACAdressRegEx,
+    isSubnetMaskRegEx
+}
